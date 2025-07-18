398ed82b17f2622940dd7963deb98c6792126bc7
hariachmad <hari08achmad@gmail.com>
2025-07-18 16:49:59 +0700

    add interval button module

---
+++ lib/module/IntervalButtonModule.h
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+class IntervalButtonModule
+{
+private:
+    int sendMessageIntervalMode = false; 
+    unsigned long lastDebounceTime = 0;
+    bool lastConfirmedState = HIGH;
+    unsigned long lowStartTime = 0;
+    unsigned long debounceDelay = 50;
+    bool waitingForConfirmation = false;
+    int pin;
+
+public:
+    IntervalButtonModule(int pinNumber);
+    void observer();
+    void begin();
+    void handleChangeState(int state);
+};
